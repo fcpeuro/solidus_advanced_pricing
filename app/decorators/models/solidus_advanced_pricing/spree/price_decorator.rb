b978634ad1356427683616303947561f37813181
Patrick McMorran <pat.mcmorran@fcpeuro.com>
2026-09-22 09:11:50 -0400

    feat: validate that valid_to falls after valid_from
    Reversed validity windows (valid_to before valid_from) were previously
    accepted. Adds a validate callback rejecting valid_to <= valid_from
    when both are present; open-ended windows (either bound nil) are
    unaffected.

--- app/decorators/models/solidus_advanced_pricing/spree/price_decorator.rb
+++ app/decorators/models/solidus_advanced_pricing/spree/price_decorator.rb
@@ -15,12 +15,20 @@ module SolidusAdvancedPricing
           optional: true
 
         base.before_validation :assign_default_price_type
+        base.validate :valid_to_after_valid_from
       end
 
       private
 
       def assign_default_price_type
         self.price_type_id ||= SolidusAdvancedPricing::PriceTypeCache.default_id
+      end
+
+      def valid_to_after_valid_from
+        return if valid_from.blank? || valid_to.blank?
+        return if valid_to > valid_from
+
+        errors.add(:valid_to, :must_be_after_valid_from)
       end
 
       ::Spree::Price.prepend self
